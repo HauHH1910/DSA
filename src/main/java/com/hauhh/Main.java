d71989df4466b328c2018f3aaafd7780441644f6
HaiHau <hhhau1910@gmail.com>
2024-11-07 14:41:14 +0700

    lengthOfLongestSubstringKDistinct

--- src/main/java/com/hauhh/Main.java
+++ src/main/java/com/hauhh/Main.java
@@ -1,8 +1,10 @@
 package com.hauhh;
 
+import static com.hauhh.Solution.lengthOfLongestSubstringKDistinct;
+
 public class Main {
     public static void main(String[] args) {
-        String[] input = {"abcabcbb", "bbbbb", "pwwkew" };
+        String[] input = {"abcabcbb", "bbbbb", "pwwkew"};
 
         System.out.println("Length of Longest Substring without repeating");
 
@@ -16,7 +18,16 @@ public class Main {
 
         System.out.println();
 
-
+        String[] input2 = {"eceba", "aa"};
+        for (String in : input2) {
+            for (int k = 1; k <= in.length(); k++) {
+                System.out.printf(
+                        "lengthOfLongestSubstringKDistinct(\"%s\", %d) = %d%n",
+                        in,
+                        k,
+                        Solution.lengthOfLongestSubstringKDistinct(in, k));
+            }
+        }
     }
 
 
--- src/main/java/com/hauhh/Solution.java
+++ src/main/java/com/hauhh/Solution.java
@@ -6,11 +6,29 @@ public class Solution {
     public static int lengthOfLongestSubstringWithoutRepeating(String s) {
         int[] map = new int[256];
         int max = 0;
-        for(int i = 0, j = 0; i < s.length(); i++){
+        for (int i = 0, j = 0; i < s.length(); i++) {
             map[s.charAt(i)]++;
-            for(; map[s.charAt(i)] > 1; j++) map[s.charAt(j)]--;
+            for (; map[s.charAt(i)] > 1; j++) map[s.charAt(j)]--;
 
             max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+
+    // PREMIUM: https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+    public static int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int[] map = new int[256];
+        int max = 0, cnt = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map[c]++;
+            if (map[c] == 1) cnt++; //Update status using s[i]
+            for (; cnt > k; j++) { // shrink the left edge while invalid
+                c = s.charAt(j);
+                map[c]--;
+                if (map[c] == 0) cnt--; // update state using s[j]
+            }
+            max = Math.max(max, i - j + 1); // longest window so far
         }
         return max;
     }
